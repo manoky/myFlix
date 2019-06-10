d21a58589bb81df83d741a12e62052ce6a2b0122
Emmanuel <emmopk11@outlook.com>
2019-06-10 21:45:20 +0200

    Add detailed meta data

--- api/index.js
+++ api/index.js
@@ -4,7 +4,14 @@ const passport = require('passport');
 const Users = require('../models/User');
 const Comments = require('../models/Comment');
 const Movies = require('../models/Movie');
+// const cloudinary = require('cloudinary');
 require('dotenv').config();
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// })
 
 const dbURL = process.env.MONGO_URL;
 
